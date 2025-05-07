2ae25e7860b21e2c19c9fa4e90c8d7718866e834
tyymr23 <99425377+tyymr23@users.noreply.github.com>
2025-05-06 22:47:55 -0400

    Fixed footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-maroon text-white text-center py-4">
-      <p>&copy; {new Date().getFullYear()} Group 9</p>
+      <p>&copy; {new Date().getFullYear()} Group 12</p>
     </footer>
   );
 };
